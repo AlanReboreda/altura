1b6ac11ee343c7a42125daef43e6f6b4bb286030
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-14 19:58:41 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let btnEnv = document.getElementById("btnEnviar");
+let altura = document.getElementById("altura");
+
+btnEnv.addEventListener("click", () => {
+const altura: number = 1.3; 
+let alturapermitida: number = dato.value;
+if(alturapersona <== alturapermitida)
+     console.log("la persona puede jugar")
+
+}else{
+  
+  console.log("la persona no puede jugar")
+}
+     
+
+
